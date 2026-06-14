0aaa056019423e78822fa31cd789f6f72e73414e
sreeja-create <alesreeja1@gmail.com>
2026-06-14 10:02:51 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -56,4 +56,19 @@ public class Main {
     }
 }
 ---------
+centigrade to fahrenhit
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int c = sc.nextInt();
+
+        int f = ((9 * c) / 5) + 32;
+
+        System.out.println("Centigrade to Fahrenheit = " + f);
+    }
+}
+---------
 
