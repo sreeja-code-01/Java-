67aa1804d8fb575af84418b4e8446a91b8cd3363
sreeja-create <alesreeja1@gmail.com>
2026-06-18 17:24:49 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,15 @@
+print no.s:-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a no ");
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
+---------
+
