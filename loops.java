eb7e3d0eef62d7eb484f2f005e778db9ea19df5e
sreeja-create <alesreeja1@gmail.com>
2026-06-18 17:52:43 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -63,3 +63,22 @@ public class Main {
     }
 }
 --------
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a no ");
+        int n=sc.nextInt();
+        int rev=0;
+        int temp=n;
+        while(n>0)
+        {
+       int d=n%10;
+       rev=rev*10+d;
+       n=n/10;
+        }
+        System.out.println(rev);
+                
+    }
+}
+------
