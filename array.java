197f14b42bc2867b24f7377bbf39ecbaeb7d03c4
sreeja-create <alesreeja1@gmail.com>
2026-06-23 17:48:46 +0530

    Update array.java

--- array.java
+++ array.java
@@ -34,3 +34,39 @@ public class Main {
     }
 }
 ----------------------
+    #arrange elements in desecding order
+    import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("enter elemnets: ");
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+    
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]<arr[j+1])
+                {
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("sorted elements");
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
+------------
